9e7a3016085ac36653ad3651109f767dda176161
Bobby Kamal <bobbykamal@hotmail.co.uk>
2021-06-23 23:51:23 +0100

    removed redundant code and added a button

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
+//Global scope
 const rollEl = document.getElementById("roll");
-
+const selectEl = document.querySelectorAll("select");
 let bonusArray = [];
+// selectEl.values;
 
 function rollDice(diceType, diceNumber) {
   // Rolling the dice, pushing it to array & sorting
   let rollArray = [];
   for (let i = 0; i < diceNumber; i++) {
-    let roll = Math.floor(Math.random() * (diceType + 1));
-    if (roll === 0) {
-      roll = 1;
-    }
+    let roll = Math.floor(Math.random() * diceType) + 1;
     rollArray.push(roll);
   }
   rollArray.sort(numberCompare);
@@ -18,19 +17,25 @@ function rollDice(diceType, diceNumber) {
   //Creating an array of real stats
   bonusArray = [];
   for (let i = 0; i < rollArray.length; i++) {
-      let statValue = rollArray[i] - 10;
-    //   statValue = Math.floor(statValue/2);
-    //   console.log(statValue);
+    let statValue = rollArray[i] - 10;
     if (statValue < 0) {
-      statValue = Math.ceil(statValue/2);
+      statValue = Math.ceil(statValue / 2);
     } else {
-      statValue = Math.floor(statValue/2);
+      statValue = Math.floor(statValue / 2);
     }
+    if (statValue === -0) {
+      statValue = 0;
+    } //This accounts for 9 returning -0
     bonusArray.push(statValue);
   }
   console.log(bonusArray);
 }
 
 function numberCompare(a, b) {
-  return a - b;
+  return b - a;
 }
+
+//Event Listeners
+rollEl.addEventListener("click", () => {
+  rollDice(20, 6);
+});
