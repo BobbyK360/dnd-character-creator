358ac11fb8ddbfebdef4b079706e280695c81fa3
Bobby Kamal <bobbykamal@hotmail.co.uk>
2021-06-29 19:48:39 +0100

    cleared up some javascript mess; changed the way repeated arrays were defined using Object.key; changed for loops for for-of loops; made sure any variable that could be a const, was a const

--- js/app.js
+++ js/app.js
@@ -53,7 +53,9 @@ function getDragAfterElement(dropzone, y) {
   ).element;
 }
 
+//======================
 //Dice & Object Function
+//======================
 
 //Roll four d6
 //Take the sum of the best 3 -> that's your number for a single stat.
@@ -63,9 +65,9 @@ function rollDice(diceType, diceNumber) {
   // Rolling the dice, pushing it to array & sorting
   rollArray.splice(0, rollArray.length);
   for (let i = 0; i < diceNumber; i++) {
-    let tempRollArray = [];
+    const tempRollArray = [];
     for (let j = 0; j < 4; j++) {
-      let roll = Math.floor(Math.random() * diceType) + 1;
+      const roll = Math.floor(Math.random() * diceType) + 1;
       tempRollArray.push(roll);
     }
     tempRollArray.sort(numberCompare);
@@ -108,16 +110,15 @@ function numberCompare(a, b) {
 const characterLevel = document.getElementById("level");
 
 characterLevel.addEventListener("input", () => {
-  let levelDisplay = document.querySelector(".level-display");
+  const levelDisplay = document.querySelector(".level-display");
   levelDisplay.innerHTML = characterLevel.value;
 });
 
 function getName() {
   characterInfo.splice(0, characterInfo.length);
-  let characterName = document.getElementById("name").value;
-  if (characterName === "") {
-    characterName = "No name";
-  }
+  const characterName = document.getElementById("name").value
+    ? document.getElementById("name").value
+    : "No Name";
   characterInfo.push(`${characterName}`);
 }
 
@@ -160,7 +161,7 @@ for (let i = 0; i < profChecks.length; i++) {
 
   function checkLimiter() {
     const selectedChecks = document.querySelectorAll(".check:checked");
-    let checksLeft = maxChecks - selectedChecks.length;
+    const checksLeft = maxChecks - selectedChecks.length;
 
     const profUnchecked = document.querySelectorAll(".check:not(:checked)");
 
@@ -188,12 +189,6 @@ characterClassContainer.addEventListener("click", () => {
   proficienciesEl.innerHTML = `${checksLeftGlobal}`;
 });
 
-// for (let i = 0; i < characterRace.length; i++) {
-//   characterRaceContainer.addEventListener("input", () => {
-//     proficienciesEl.innerHTML = `${checksLeftGlobal}`;
-//   })
-// }
-
 //Event Listeners
 rollEl.addEventListener("click", () => {
   getName();
@@ -204,7 +199,7 @@ rollEl.addEventListener("click", () => {
   getProficiencies();
   returnAttPriorityArray();
   rollDice(6, 6);
-  let CharacterObject = new Character();
+  const CharacterObject = new Character();
   characterArray.push(CharacterObject);
   console.table(characterArray);
   assignOutputFieldHeader(CharacterObject);
@@ -239,6 +234,10 @@ class Character {
   }
 }
 
+//==========================
+//Assign values to the sheet
+//==========================
+
 //Functions to assign values to the sheet
 //Because calling a bunch of the same assignment functions is ugly, here is a function that runs through all
 //the output boxes and assigns the innerHTML of the associated span. The only thing hard-coded is the fieldListHeaderArray
@@ -254,36 +253,17 @@ function assignOutputFieldHeader(object) {
     "faction",
     "exp",
   ];
-  for (i = 0; i < fieldList.length; i++) {
+
+  for (const attribute of fieldList) {
     const outputGrabber = document.querySelector(
-      `.sheet__character-${fieldList[i]} h1 span`
+      `.sheet__character-${attribute} h1 span`
     );
-    outputGrabber.innerHTML = object[fieldList[i]];
+    outputGrabber.innerHTML = object[attribute];
   }
+
   //Set the player name separately as it follows a different naming convention.
   const playerName = document.querySelector(`.sheet__player-name h1 span`);
   playerName.innerHTML = object[playerName];
-}
-
-function assignOutputFieldStats(object) {
-  const fieldList = [
-    "strength",
-    "dexterity",
-    "constitution",
-    "intelligence",
-    "wisdom",
-    "charisma",
-  ];
-  for (i = 0; i < fieldList.length; i++) {
-    const outputGrabberBonus = document.querySelector(
-      `.sheet__character-${fieldList[i]} span:first-of-type`
-    );
-    const outputGrabberOrig = document.querySelector(
-      `.sheet__character-${fieldList[i]} span:last-child`
-    );
-    outputGrabberBonus.innerHTML = object[fieldList[i]];
-    outputGrabberOrig.innerHTML = object[`${fieldList[i]}Orig`];
-  }
 }
 
 const skillsModifier = {
@@ -295,6 +275,22 @@ const skillsModifier = {
   charisma: ["deception", "intimidation", "performance", "persuasion"],
 };
 
+function assignOutputFieldStats(object) {
+  const fieldList = Object.keys(skillsModifier);
+
+  for (const attribute of fieldList) {
+    const outputGrabberBonus = document.querySelector(
+      `.sheet__character-${attribute} span:first-of-type`
+    );
+    const outputGrabberOrig = document.querySelector(
+      `.sheet__character-${attribute} span:last-child`
+    );
+
+    outputGrabberBonus.innerHTML = object[attribute];
+    outputGrabberOrig.innerHTML = object[`${attribute}Orig`];
+  }
+}
+
 function assignSkills(object) {
   const skillsElArray = Array.from(
     document.querySelectorAll(".sheet__character-skills > div")
@@ -303,62 +299,22 @@ function assignSkills(object) {
   const skillsArraySpan = Array.from(
     document.querySelectorAll(".sheet__character-skills > div h1 span")
   );
-  for (i = 0; i < skillsElArray.length; i++) {
-    const skillsElValue = skillsElArray[i].dataset.value;
-    skillsArray.push(skillsElValue);
+  const fieldList = Object.keys(skillsModifier);
+
+  for (const pushValue of skillsElArray) {
+    skillsArray.push(pushValue.dataset.value);
   }
-  console.log(skillsArray);
 
-  const fieldList = [
-    "strength",
-    "dexterity",
-    "constitution",
-    "intelligence",
-    "wisdom",
-    "charisma",
-  ];
+  for (const attribute of fieldList) {
+    for (const skill of skillsModifier[`${attribute}`]) {
+      if (skillsArray.indexOf(skill) > -1) {
+        const skillsIndexer = skillsArray.indexOf(skill);
 
-  for (let attribute of fieldList) {
-    for (var i = 0; i < skillsModifier[`${attribute}`].length; i++) {
-      let indexer = skillsModifier[`${attribute}`][i];
-      if (skillsArray.indexOf(indexer) > -1) {
-  
-        let skillsIndexer = skillsArray.indexOf(indexer);
-        console.log(skillsIndexer);
-  
         skillsArraySpan[skillsIndexer].innerHTML = object[`${attribute}`];
-        
+
         skillsArraySpan.splice(skillsIndexer, 1);
         skillsArray.splice(skillsIndexer, 1);
       }
     }
   }
-
-  for (var i = 0; i < skillsModifier["intelligence"].length; i++) {
-    let indexer = skillsModifier["intelligence"][i];
-    if (skillsArray.indexOf(indexer) > -1) {
-
-      let skillsIndexer = skillsArray.indexOf(indexer);
-      console.log(skillsIndexer);
-
-      skillsArraySpan[skillsIndexer].innerHTML = object["intelligence"];
-      
-      skillsArraySpan.splice(skillsIndexer, 1);
-      skillsArray.splice(skillsIndexer, 1);
-    }
-  }
-  console.log(skillsArray);
-  console.log(skillsArraySpan);
-
-
-  //loop over strength array
-  //if any matches:
-    //find the index of all strength matches in skillsArray
-    //slice and discard values of skills array
-    //move on to dexterity
-    //etc
-  //if no matches
-    //move on to dexterity
-    //repeat
-
 }
